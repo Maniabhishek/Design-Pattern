0c4f00794cfed15c8a03fa36421e5e7363438450
Maniabhishek <31520295+Maniabhishek@users.noreply.github.com>
2024-09-27 19:00:27 +0530

    update main

--- code/TS/main.ts
+++ code/TS/main.ts
@@ -1,8 +1,10 @@
+import { Call_decorator } from "./decoratorPattern"
+import { Call_executeObserverPattern } from "./observerPattern"
 import { SportsVehicle } from "./strategyPattern/withStrategyPattern/sportsVehicle"
 
 function main(){
-    let sportV = new SportsVehicle()
-    sportV.sportsDrive()
+    // Call_executeObserverPattern()
+    Call_decorator()
 }
 
 main()
