9132312e4c2672354cb1f33f71b34239079c46ea
Maniabhishek <31520295+Maniabhishek@users.noreply.github.com>
2026-03-11 22:52:12 +0530

    Create userBuilder.ts

---
+++ 4.CreationalDesignPattern/d.builder/example2/userBuilder.ts
@@ -0,0 +1,59 @@
+import { User } from "./user.js";
+
+export abstract class UserBuilder {
+    id: string;
+    name? : string;
+    email? : string;
+    age? : number;
+    dob? : string;
+    height? : number;
+    weight? : number;
+    picture? : string;
+    previlege?: string[];
+    constructor(id: string){
+        this.id = id
+    }
+
+    setName(name: string): UserBuilder{
+        this.name = name
+        return this
+    }
+
+    setEmail(email: string): UserBuilder{
+        this.email = email
+        return this
+    }
+
+    setAge(age: number): UserBuilder{
+        this.age = age
+        return this
+    }
+
+    setDob(dob: string): UserBuilder{
+        this.dob = dob
+        return this
+    }
+
+    setHeight(height: number): UserBuilder{
+        this.height = height
+        return this
+    }
+
+    setWeight(weight: number): UserBuilder{
+        this.weight = weight
+        return this
+    }
+
+    setPicture(picture: string): UserBuilder{
+        this.picture = picture
+        return this
+    }
+
+    abstract setPrevilege(previlege: string[]): UserBuilder;
+
+    build(){
+        return new User(
+            this.id, this.name, this.email, this.age, this. dob, this.height, this.weight, this.picture, this.previlege
+        )
+    }
+}
