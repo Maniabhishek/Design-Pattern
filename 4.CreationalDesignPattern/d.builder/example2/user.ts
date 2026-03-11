5228d6e33763cecf674b8212fb2dc62bb806ad4b
Maniabhishek <31520295+Maniabhishek@users.noreply.github.com>
2026-03-11 22:51:50 +0530

    Create user.ts

---
+++ 4.CreationalDesignPattern/d.builder/example2/user.ts
@@ -0,0 +1,26 @@
+export class User {
+    id: string;
+    name?: string | undefined;
+    email?: string | undefined;
+    age?: number | undefined;
+    dob?: string | undefined;
+    height?: number | undefined;
+    weight?: number | undefined;
+    picture?: string | undefined;  
+    previlege?: string[] | undefined
+    constructor(id: string, name?: string, email?: string, age?: number, dob?: string, height?: number, weight?: number, picture?: string, previlege?: string[]){
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.age = age;
+        this.dob = dob;
+        this.height = height;
+        this.weight = weight;
+        this.picture = picture;
+        this.previlege = previlege
+    }
+
+    toString(){
+        console.log(this.email, ' : ', this.age, ' : ', this.previlege)
+    }
+}
